d12484bd797ffe036435c559acad4c2fb9590813
Carlos Rueda <carueda@mbari.org>
2021-09-29 18:32:21 -0700

    have apply_model accept intervals from command line

--- hwsd/apply_model.py
+++ hwsd/apply_model.py
@@ -1,62 +1,78 @@
 #!/usr/bin/env python3
 """
-Main script to run apply_model_day as needed.
-No options from command line; adjust the script itself.
-     $ nohup python3 -u hwsd/apply_model.py &
-     $ tail -f nohup.out
+A main script to run apply_model_day on given time intervals.
+See USAGE.
 """
 
+import sys
 import time
+from typing import List
 
-from hwsd import file_helper
 from hwsd.apply_model_day import apply_model_day
+from hwsd.file_helper import FileHelper, DEFAULT_SCORE_BASE_DIR
 from hwsd.misc import elapsed_end, parse_days
 from hwsd.model_helper import ModelHelper
 
+# Adjust the following depending on cpu/ram resources available to apply the model:
+HOURS_PER_CALL = 3  # how many hours of audio to keep in memory
+MODEL_MINUTES = 60  # Size of audio to pass to the model.
+# The longer this is the more resources used by the model.
 
-def main() -> None:
-    "Main program."
 
-    # Adjust as needed (the intervals are adjusted by parse_days):
-    intervals = ["2018/11-12/1-31", "2019/1-12/1-31", "2020/1-10/1-31"]
-    # intervals = ["2018/12/1-15"]
-    # intervals = ["2018/12/16-31"]
+USAGE = """
+hwsd/apply_model.py: A main script to apply the model on given time intervals.
+
+Usage:
+    $ hwsd/apply_model.py time-interval ...
+  where each time interval must be of the form 'yearRange/monthRange/dayRange'
+  with each __Range either a single number or a hyphen-separated range with inclusive limits.
+
+Example: Apply the model on the six full months Oct–Dec'2020 and Jan–Mar'2021:
+    $ hwsd/apply_model.py "2020/10-12/1-31" "2021/1-3/1-31"
+
+Some of our runs on gizo were like this (two concurrent jobs to process Jan–Aug'2021):
+    $ source virtenv/bin/activate
+    $ nohup python3 -u hwsd/apply_model.py "2021/1-4/1-31" > nohup-2021--1-4.out &
+    $ nohup python3 -u hwsd/apply_model.py "2021/5-8/1-31" > nohup-2021--5-8.out &
+"""
+
+
+def main(intervals: List[str]) -> None:
+    """
+    Applies the model on the given intervals.
+    """
+
     years_months_days = parse_days(*intervals)
 
     print(f"\nSTARTING apply_model with intervals={intervals}")
     program_started = time.time()
 
-    # Adjust the following depending on cpu/ram resources available
-    # to apply the model:
-    hours_per_call = 3  # how many hours of audio to keep in memory
-    model_minutes = 60  # Size of audio to pass to the model.
-    # The longer this is the more resources used by the model.
-
     # With 10kHz already pre-generated:
     audio_base_dir = "/PAM_Analysis/GoogleHumpbackModel/decimated_10kHz"
     # (The default in file_helper is to read from the 16kHz data.)
-
-    score_base_dir = file_helper.DEFAULT_SCORE_BASE_DIR
 
     model_helper = ModelHelper()
     model_helper.load_model()
 
     for year, month, day in years_months_days:
         print(f"\n*** DAY {year:04}-{month:02}-{day:02} ***")
-        for at_hour in range(0, 24, hours_per_call):
+        for at_hour in range(0, 24, HOURS_PER_CALL):
             apply_model_day(
-                file_helper.FileHelper(audio_base_dir, score_base_dir),
+                FileHelper(audio_base_dir, DEFAULT_SCORE_BASE_DIR),
                 model_helper,
                 year,
                 month,
                 day,
                 at_hour=at_hour,
-                hours=hours_per_call,
-                model_minutes=model_minutes,
+                hours=HOURS_PER_CALL,
+                model_minutes=MODEL_MINUTES,
             )
 
     print(f"\n>> complete apply_model in {elapsed_end(program_started)}\n")
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        print(USAGE)
