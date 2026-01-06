f05fe1ac118a42fafeabdd50d163dac2550c403f
Carlos Rueda <carueda@mbari.org>
2026-01-06 15:02:25 -0800

    adjust resample_sox.sh to allow specific days in the month

--- resample_sox.sh
+++ resample_sox.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
 
-# This script runs `sox` for resampling all days in a given year/month,
-# which are required arguments. Example:
-#    ./resample_sox.sh 2018 11
+# This script runs `sox` for resampling all given days in a given year and month.
 # Each resample is launched in its own process.
 
 set -ue
 
+if [[ $# -lt 2 ]]; then
+  echo "Usage: $0 <year> <month> [ <days>... ]"
+  echo "Examples:"
+  echo "  $0 2018 11              # all days by default"
+  echo "  $0 2025 8 \$(seq 21 31)  # days 21-31"
+  exit 1
+fi
+
 year=$1
 month=$2
-days=$(seq 1 31)  # for convenience 1–31 regardless of month
+shift 2
+days=$*
+if [[ -z "$days" ]]; then
+  days=$(seq 1 31)  # for convenience 1–31 regardless of month
+fi
+
+days_line="$(echo "${days}" | tr '\n' ' ')"
+printf "Starting resample_sox.sh: %04d-%02d days: %s\n" "${year}" "${month}" "${days_line}"
 
 audio_base_dir="/mnt/PAM_Analysis/decimated_16kHz"
 decimated_base_dir="/mnt/PAM_Analysis/GoogleHumpbackModel/decimated_10kHz"
 
-days_line="$(echo "${days}" | tr '\n' ' ')"
-
 in_dir=$(printf "%s/%04d/%02d" ${audio_base_dir} "${year}" "${month}")
 out_dir=$(printf "%s/%04d/%02d" ${decimated_base_dir} "${year}" "${month}")
 mkdir -p "${out_dir}"
-
-printf "Starting resample_sox.sh: %04d-%02d days: %s\n" "${year}" "${month}" "${days_line}"
 
 for day in ${days}; do
   infile=$(printf "%s/MARS-%04d%02d%02dT000000Z-16kHz.wav" "${in_dir}" "${year}" "${month}" "${day}")
@@ -30,3 +39,5 @@ done
 wait
 
 printf "\nCompleted resample_sox.sh for %04d-%02d days:%s\n" "${year}" "${month}" "${days_line}"
+month_dir=$(printf "%s/%04d/%02d" "${decimated_base_dir}" "${year}" "${month}")
+echo "Month directory: ${month_dir}"
